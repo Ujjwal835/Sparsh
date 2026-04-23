1c1dc0abd80e1e4499a0aa445552b4a6ffc1236d
Ujjwal <ujjwaljindal835@gmail.com>
2026-04-23 23:05:48 +0530

    created folder structure in accordance wit scalability

---
+++ prisma/seed.ts
@@ -0,0 +1,33 @@
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+async function main() {
+  await prisma.product.create({
+    data: {
+      name: "Nike Air Max",
+      brand: "Nike",
+      images: ["https://via.placeholder.com/300"],
+      variants: {
+        create: [
+          {
+            size: 8,
+            color: "Black",
+            price: 4999,
+            stock: 10,
+            sku: "NAM-BLK-8",
+          },
+          {
+            size: 9,
+            color: "Black",
+            price: 4999,
+            stock: 15,
+            sku: "NAM-BLK-9",
+          },
+        ],
+      },
+    },
+  });
+}
+
+main();
